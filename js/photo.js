13102dbf55671fa631d57cb0c28f10e38917ae70
sunnybairagee <getprimehelp@gmail.com>
2026-01-22 22:06:49 +0530

    Update photo.js

--- js/photo.js
+++ js/photo.js
@@ -1,1 +1,9 @@
+const params = new URLSearchParams(window.location.search);
+const img = params.get("id");
 
+const image = document.getElementById("photo");
+image.src = `images/${img}`;
+
+const download = document.getElementById("download");
+download.href = `images/${img}`;
+download.download = img;
