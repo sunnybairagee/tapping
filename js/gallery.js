c3a9fcf7e2b579eb1adc76a7142105a8047125fa
sunnybairagee <getprimehelp@gmail.com>
2026-01-22 22:06:27 +0530

    Fetch and display images from JSON data

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,19 @@
+fetch("data/images.json")
+  .then(res => res.json())
+  .then(images => {
+    document.getElementById("count").innerText = `Total Photos: ${images.length}`;
 
+    const gallery = document.getElementById("gallery");
+
+    images.forEach(img => {
+      const a = document.createElement("a");
+      a.href = `photo.html?id=${encodeURIComponent(img)}`;
+
+      const image = document.createElement("img");
+      image.src = `images/${img}`;
+      image.loading = "lazy";
+
+      a.appendChild(image);
+      gallery.appendChild(a);
+    });
+  });
