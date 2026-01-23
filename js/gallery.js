9bf632ea4c3accfb5fad831436d12e2d3b98a6ed
sunnybairagee <getprimehelp@gmail.com>
2026-01-23 18:26:36 +0530

    Enhance gallery with lightbox and image click handling
    Refactored gallery image display logic and added lightbox functionality.

--- js/gallery.js
+++ js/gallery.js
@@ -1,20 +1,58 @@
 fetch("data/images.json")
   .then(res => res.json())
   .then(images => {
-    images.reverse();
-    document.getElementById("count").innerText = `Total Photos: ${images.length}`;
+    images.reverse(); // latest first
 
     const gallery = document.getElementById("gallery");
+    document.getElementById("count").innerText = `Total Photos: ${images.length}`;
 
     images.forEach(img => {
-      const a = document.createElement("a");
-      a.href = `photo.html?id=${encodeURIComponent(img)}`;
+      const div = document.createElement("div");
+      div.className = "gallery-item";
 
       const image = document.createElement("img");
       image.src = `images/${img}`;
       image.loading = "lazy";
 
-      a.appendChild(image);
-      gallery.appendChild(a);
+      image.onclick = () => openLightbox(img);
+
+      div.appendChild(image);
+      gallery.appendChild(div);
     });
   });
+
+/* Lightbox logic */
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const downloadBtn = document.getElementById("downloadBtn");
+
+function openLightbox(img) {
+  lightbox.style.display = "flex";
+  lightboxImg.src = `images/${img}`;
+  downloadBtn.href = `images/${img}`;
+}
+
+document.getElementById("close").onclick = () => {
+  lightbox.style.display = "none";
+};
+
+// fetch("data/images.json")
+//   .then(res => res.json())
+//   .then(images => {
+//     images.reverse();
+//     document.getElementById("count").innerText = `Total Photos: ${images.length}`;
+
+//     const gallery = document.getElementById("gallery");
+
+//     images.forEach(img => {
+//       const a = document.createElement("a");
+//       a.href = `photo.html?id=${encodeURIComponent(img)}`;
+
+//       const image = document.createElement("img");
+//       image.src = `images/${img}`;
+//       image.loading = "lazy";
+
+//       a.appendChild(image);
+//       gallery.appendChild(a);
+//     });
+//   });
